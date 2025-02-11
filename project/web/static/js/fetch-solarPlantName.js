aa60d570c036f637b132c258a8a4f5d1fd98eef8
Chalisa Thummaraj <6410685041@student.tu.ac.th>
2025-02-11 17:34:14 +0700

    manage script file for fetching data

---
+++ project/web/static/js/fetch-solarPlantName.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function(){
+    fetch('http://127.0.0.1:5003/SolarPlant/allname')
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Network response was not ok: ' + response.statusText);
+      }
+      return response.json();
+  })
+  .then(string_data => {
+      try {
+          const data = JSON.parse(string_data);
+          const selectElement = document.getElementById('solarPlant');
+
+          if (Array.isArray(data)) {
+              data.forEach(item => {
+                  const option = document.createElement('option');
+                  option.value = item.solarPlant_name; // Correct property name
+                  option.textContent = item.solarPlant_name;
+                  selectElement.appendChild(option);
+              });
+          } else {
+              console.error('Expected an array but received:', data);
+          }
+      } catch (error) {
+          console.error('Error parsing JSON:', error);
+      }
+    })
+    .catch(error => console.error('Error fetching data:', error.message));
+  });
